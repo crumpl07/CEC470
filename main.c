2a4e24d6534f32d415ed2f1ce1c2e6b628415f3e
crumpl07 <CRUMPST3R2000@GMAIL.COM>
2021-11-04 12:17:39 -0400

    Started Code

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
-//her be the code
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HALT_OPCODE 0x19
+
+//Function Declarations
+void fetchNextInstruciton(void);
+void executeInstruction(void);
+
+//Global Variables
+unsigned int MAR = 0;
+unsigned int PC = 0;
+unsigned char memory[65536];
+unsigned char ACC = 0;
+unsigned char IR = 0;
+
+int main (int argc, char* argv[])
+{
+    while(memory[PC] != HALT_OPCODE)
+    {
+        fetchNextInstruciton();
+        executeInstruction();
+    }
+    return 0;
+}
